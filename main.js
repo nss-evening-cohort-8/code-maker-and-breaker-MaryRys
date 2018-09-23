c611acf3dbbd998079d8437ea8064e22414ca40b
Mary Rys <rys.mary@gmail.com>
2018-09-23 11:59:25 -0500

    finished translate

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-const codeOutput = document.getElementById("codeOutput");
-
-const makeCode = () => {
-const inputValue = document.getElementById('inputCodeMaker').value;
-const inputCode = inputValue.split(" ");
-const inputCodeString = makeCodeStringBuilder(inputCode);
-printToDom(inputCodeString, 'codeOutput')
-}
-
-const makeCodeStringBuilder = (inputCode) => {
-    let newString = '';
-    for (let i = 0; i < inputCode.length; i ++) {
-    let codeWord = inputCodeMaker[inputCode[i]]
-    newString += `<p> ${codeWord}</p>`
-    };
-    return newString;
-}
-
-const makeCodeButton = document.getElementById("makeCodeButton");
-makeCodeButton.addEventListener ('click', makeCode);
-
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+const makeCodeButton = document.getElementById("makeCodeButton");
+makeCodeButton.addEventListener('click', () => {
+    const inputValue = document.getElementById('inputCodeMaker').value;
+    const inputCode = inputValue.split("");
+    for (let i = 0; i < inputCode.length; i++) {
+        const secretCode = inputValue.charCodeAt(inputCode[i]);
+        printToDom(secretCode, "codeOutput")
+    };
+});
